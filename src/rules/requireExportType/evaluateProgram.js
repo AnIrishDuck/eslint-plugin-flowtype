12c16f4dd1dc7d969036872e1091b2d2697bff83
Frank Murphy <fmurphy@instructure.com>
2017-03-13 15:30:15 -0400

    Pull out evaluateProgram

---
+++ src/rules/requireExportType/evaluateProgram.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+import {quoteName} from './../../utilities';
+import evaluateFunction from './evaluateFunction';
+import evaluateVariable from './evaluateVariable';
+
+const isExport = (node) => {
+  return node.type === 'ExportNamedDeclaration';
+};
+
+const requiredBelow = (prefix) => {
+  return prefix + ', required by export below.';
+};
+
+const reportExport = function (context, specifier, priorLine) {
+  context.report({
+    data: {
+      name: quoteName(specifier.local.name),
+      priorLine
+    },
+    message: 'Missing or incomplete type annotation on prior {{name}}declaration at line {{priorLine}}.',
+    node: specifier
+  });
+};
+
+export default function (context) {
+  return (programNode) => {
+    const exportNodes = programNode.body.filter(isExport);
+    const nodePairs = exportNodes.map((node) => {
+      return node.specifiers.map((specifier) => {
+        return [specifier.local.name, specifier];
+      });
+    });
+    const exportMap = new Map(_.flatten(nodePairs));
+
+    programNode.body.forEach((node) => {
+      if (node.type === 'VariableDeclaration') {
+        node.declarations.forEach((declarator) => {
+          const name = declarator.id.name;
+          const specifier = exportMap.get(name);
+
+          if (specifier) {
+            if (!evaluateVariable(context, declarator, requiredBelow)) {
+              reportExport(context, specifier, node.loc.start.line);
+            }
+          }
+        });
+      } else if (node.type === 'FunctionDeclaration') {
+        const name = node.id ? node.id.name : null;
+        const specifier = exportMap.get(name);
+
+        if (specifier && !evaluateFunction(context, node, requiredBelow)) {
+          reportExport(context, specifier, node.loc.start.line);
+        }
+      }
+    });
+  };
+}
--- src/rules/requireExportType/index.js
+++ src/rules/requireExportType/index.js
@@ -1,27 +1,7 @@
 import _ from 'lodash';
-import {isFlowFile, quoteName} from './../../utilities';
+import {isFlowFile} from './../../utilities';
 import evaluateExport from './evaluateExport';
-import evaluateFunction from './evaluateFunction';
-import evaluateVariable from './evaluateVariable';
-
-const isExport = (node) => {
-  return node.type === 'ExportNamedDeclaration';
-};
-
-const requiredBelow = (prefix) => {
-  return prefix + ', required by export below.';
-};
-
-const reportExport = function (context, specifier, priorLine) {
-  context.report({
-    data: {
-      name: quoteName(specifier.local.name),
-      priorLine
-    },
-    message: 'Missing or incomplete type annotation on prior {{name}}declaration at line {{priorLine}}.',
-    node: specifier
-  });
-};
+import evaluateProgram from './evaluateProgram';
 
 export default (context) => {
   const checkThisFile = !_.get(context, 'settings.flowtype.onlyFilesWithFlowAnnotation') || isFlowFile(context);
@@ -33,36 +13,6 @@ export default (context) => {
   return {
     ExportDefaultDeclaration: evaluateExport(context),
     ExportNamedDeclaration: evaluateExport(context),
-    Program: (programNode) => {
-      const exportNodes = programNode.body.filter(isExport);
-      const nodePairs = exportNodes.map((node) => {
-        return node.specifiers.map((specifier) => {
-          return [specifier.local.name, specifier];
-        });
-      });
-      const exportMap = new Map(_.flatten(nodePairs));
-
-      programNode.body.forEach((node) => {
-        if (node.type === 'VariableDeclaration') {
-          node.declarations.forEach((declarator) => {
-            const name = declarator.id.name;
-            const specifier = exportMap.get(name);
-
-            if (specifier) {
-              if (!evaluateVariable(context, declarator, requiredBelow)) {
-                reportExport(context, specifier, node.loc.start.line);
-              }
-            }
-          });
-        } else if (node.type === 'FunctionDeclaration') {
-          const name = node.id ? node.id.name : null;
-          const specifier = exportMap.get(name);
-
-          if (specifier && !evaluateFunction(context, node, requiredBelow)) {
-            reportExport(context, specifier, node.loc.start.line);
-          }
-        }
-      });
-    }
+    Program: evaluateProgram(context)
   };
 };
